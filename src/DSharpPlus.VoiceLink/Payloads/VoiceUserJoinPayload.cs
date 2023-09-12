5f1b6e9e5ea563e98795fa0df2dddd3b3168b3a3
Lunar Starstrum <lunar@forsaken-borders.net>
2023-09-12 00:34:10 -0500

    Remove all lib code and start fresh

--- src/DSharpPlus.VoiceLink/Payloads/VoiceUserJoinPayload.cs
+++
@@ -1,9 +0,0 @@
-using Newtonsoft.Json;
-
-namespace DSharpPlus.VoiceLink.Payloads
-{
-    public sealed record VoiceUserJoinPayload(
-        [property: JsonProperty("user_id")] ulong UserId,
-        [property: JsonProperty("audio_ssrc")] uint Ssrc
-    );
-}
--- src/DSharpPlus.VoiceLink/VoiceGatewayDispatch.cs
+++
@@ -1,7 +0,0 @@
-using DSharpPlus.VoiceLink.Enums;
-using Newtonsoft.Json;
-
-namespace DSharpPlus.VoiceLink
-{
-    public sealed record VoiceGatewayDispatch([property: JsonProperty("op")] VoiceOpCode OpCode, [property: JsonProperty("d")] object? Data);
-}
