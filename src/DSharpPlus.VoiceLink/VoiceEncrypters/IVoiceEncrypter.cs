11135732361cb54fd164212310887c6094ebd94b
Lunar Starstrum <lunar@forsaken-borders.net>
2024-01-14 00:16:16 -0600

    All voice encryptors are now decrypting correctly

--- src/DSharpPlus.VoiceLink/VoiceEncrypters/IVoiceEncrypter.cs
+++ src/DSharpPlus.VoiceLink/VoiceEncrypters/IVoiceEncrypter.cs
@@ -1,5 +1,6 @@
 using System;
 using DSharpPlus.VoiceLink.Enums;
+using DSharpPlus.VoiceLink.Rtp;
 
 namespace DSharpPlus.VoiceLink.VoiceEncrypters
 {
@@ -12,6 +13,6 @@ namespace DSharpPlus.VoiceLink.VoiceEncrypters
         int GetDecryptedSize(int length);
         bool TryEncryptOpusPacket(VoiceLinkUser voiceLinkUser, ReadOnlySpan<byte> data, ReadOnlySpan<byte> key, Span<byte> target);
         bool TryDecryptOpusPacket(VoiceLinkUser voiceLinkUser, ReadOnlySpan<byte> data, ReadOnlySpan<byte> key, Span<byte> target);
-        bool TryDecryptReportPacket(ReadOnlySpan<byte> data, ReadOnlySpan<byte> key, Span<byte> target) => false;
+        bool TryDecryptReportPacket(RtcpHeader header, ReadOnlySpan<byte> data, ReadOnlySpan<byte> key, Span<byte> target) => false;
     }
 }
