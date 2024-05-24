b9e94b6303f51ef98439fa0bcb0a2f7cf71b1d83
Lunar Starstrum <lunar@forsaken-borders.net>
2024-05-24 17:14:24 -0500

    Do the check first

--- .github/scripts/package-opus.sh
+++ .github/scripts/package-opus.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Check if the output file exists and if we've already built it for this version
+EXPORT_DIR="$WORKSPACE/libs/libopus/$RID/native"
+if [ -f "$EXPORT_DIR/$FILE" ]; then
+  COMMIT_MESSAGE="$(git log -1 --pretty=%B -- "$EXPORT_DIR/$FILE")"
+  if [[ "$COMMIT_MESSAGE" == *"$OPUS_VERSION"* ]]; then
+    echo "Already built $FILE for $OPUS_VERSION"
+    exit 0
+  fi
+fi
+
 # Create Opus dir
 mkdir -p "$WORKSPACE/libs/opus"
 cd "$WORKSPACE/libs/opus"
@@ -12,24 +22,12 @@ git fetch --tags
 OPUS_VERSION="$(git describe --tags $(git rev-list --tags --max-count=1))"
 echo "OPUS_VERSION=$OPUS_VERSION" >> $GITHUB_ENV
 
-# Check if the output file exists and if we've already built it for this version
-EXPORT_DIR="$WORKSPACE/libs/libopus/$RID/native/"
-if [ -f "$EXPORT_DIR/$FILE" ]; then
-  COMMIT_MESSAGE="$(git log -1 --pretty=%B -- "$EXPORT_DIR/$FILE")"
-  if [[ "$COMMIT_MESSAGE" == *"$OPUS_VERSION"* ]]; then
-    echo "Already built $FILE for $OPUS_VERSION"
-    exit 0
-  fi
-fi
-
 # Checkout the latest tag
 git checkout "$OPUS_VERSION"
 
 # Build the library
 cmake -S . -B build $COMMAND_ARGS -DOPUS_BUILD_SHARED_LIBRARY=ON -DCMAKE_BUILD_TYPE=Release -Wno-dev
 cmake --build build --config Release
-
-EXPORT_DIR="$WORKSPACE/libs/libopus/$RID/native/"
 
 # Move the output file to the correct location
 mkdir -p              "$EXPORT_DIR"
--- .github/scripts/package-sodium.sh
+++ .github/scripts/package-sodium.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Check if the output file exists and if we've already built it for this version
+EXPORT_DIR="$WORKSPACE/libs/libsodium/$RID/native"
+if [ -f "$EXPORT_DIR/$FILE" ]; then
+  COMMIT_MESSAGE="$(git log -1 --pretty=%B -- "$EXPORT_DIR/$FILE")"
+  if [[ "$COMMIT_MESSAGE" == *"$SODIUM_VERSION"* ]]; then
+    echo "Already built $FILE for $SODIUM_VERSION"
+    exit 0
+  fi
+fi
+
 # Create Sodium dir
 mkdir -p "$WORKSPACE/libs/sodium"
 cd "$WORKSPACE/libs/sodium"
@@ -11,16 +21,6 @@ git fetch --tags
 # Export the latest tag
 SODIUM_VERSION="$(git describe --tags $(git rev-list --tags --max-count=1))"
 echo "SODIUM_VERSION=$SODIUM_VERSION" >> $GITHUB_ENV
-
-# Check if the output file exists and if we've already built it for this version
-EXPORT_DIR="$WORKSPACE/libs/libsodium/$RID/native/"
-if [ -f "$EXPORT_DIR/$FILE" ]; then
-  COMMIT_MESSAGE="$(git log -1 --pretty=%B -- "$EXPORT_DIR/$FILE")"
-  if [[ "$COMMIT_MESSAGE" == *"$SODIUM_VERSION"* ]]; then
-    echo "Already built $FILE for $SODIUM_VERSION"
-    exit 0
-  fi
-fi
 
 # Checkout the latest tag
 git checkout "$SODIUM_VERSION"
