a4bb324adc8a76a362141384d72e0483cc3a9e2b
Lunar Starstrum <lunar@forsaken-borders.net>
2024-06-03 15:09:00 -0500

    Remove encryption mode enum; Rename voice encryption cipher folder

--- src/DSharpPlus.VoiceLink/Enums/EncryptionMode.cs
+++
@@ -1,9 +0,0 @@
-namespace DSharpPlus.VoiceLink.Enums
-{
-    public enum EncryptionMode
-    {
-        XSalsa20Poly1305 = 1,
-        XSalsa20Poly1305Suffix = 2,
-        XSalsa20Poly1305Lite = 3
-    }
-}
